07b684341075cb98115b25814ffc0b539166e07a
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-13 16:17:12 +0300

    sidebar fix

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -49,9 +49,7 @@ export function AppSidebar() {
                       className="flex items-center gap-3 px-4 py-2.5 rounded-lg transition-all text-slate-600 hover:bg-slate-100 hover:text-slate-900"
                     >
                       <item.icon className="h-5 w-5 stroke-[1.75] " />
-                      <span className="font-2xl text-black ">
-                        {item.title}
-                      </span>
+                      <span className="font-2xl text-black ">{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
