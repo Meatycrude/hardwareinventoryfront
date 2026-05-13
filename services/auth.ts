370081a15fdf123fd99a05c6320bc3135186151b
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-13 14:09:25 +0300

    authentication service inside services

---
+++ services/auth.ts
@@ -0,0 +1,22 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function login(email: string, password: string) {
+  const response = await fetch(`${API_URL}/login`, {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Invalid credentials");
+  }
+
+  return response.json();
+}
