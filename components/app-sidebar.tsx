3a3b2199e0f81e21d4abfcf61a22d7bceba150ff
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-15 18:03:07 +0300

    logout updated and is now working

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // components/app-sidebar.tsx
 import {
   Home,
@@ -21,6 +23,10 @@ import {
   SidebarFooter,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { logout } from "@/services/auth";
 
 const items = [
   { title: "Dashboard", url: "/dashboard", icon: Home },
@@ -33,6 +39,35 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const { logoutUser } = useAuth();
+
+  async function handleLogout(e: React.FormEvent) {
+    e.preventDefault();
+
+    setLoading(true);
+    setError("");
+
+    try {
+      await logout();
+
+      logoutUser();
+
+      router.push("/login");
+
+      setLoading(false);
+    } catch (error) {
+      setError("Invalid credentials" + error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <Sidebar variant="sidebar" collapsible="icon" className="bg-slate-500">
       <SidebarContent className="bg-[#E6FEEE] pt-10">
@@ -64,7 +99,10 @@ export function AppSidebar() {
       <SidebarFooter className="p-4 border-t border-slate-100">
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton className="text-rose-600 hover:bg-rose-50 hover:text-rose-700 w-full gap-3">
+            <SidebarMenuButton
+              onClick={handleLogout}
+              className="text-rose-600 hover:bg-rose-50 hover:text-rose-700 w-full gap-3"
+            >
               <LogOut className="h-5 w-5" />
               <span>Logout</span>
             </SidebarMenuButton>
